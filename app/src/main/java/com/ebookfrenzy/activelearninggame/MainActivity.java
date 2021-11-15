cf86afc2cc6d24da4f38ce84a0eaecec8a774053
ghadasaeed <1181302867@student.mmu.edu.my>
2021-11-15 22:34:27 +0300

    Add intro page

--- app/src/main/java/com/ebookfrenzy/activelearninggame/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/activelearninggame/MainActivity.java
@@ -1,14 +1,21 @@
 package com.ebookfrenzy.activelearninggame;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
@@ -18,30 +25,34 @@ public class MainActivity extends AppCompatActivity {
 
     Button Easy, Medium, Hard;
     Intent intent;
+    ImageView imageView;
+    TextView textView;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.intro);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView7);
+
+        imageView.animate().alpha(0f).setDuration(0);
+        textView.animate().alpha(0f).setDuration(0);
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
         setContentView(R.layout.activity_choose_one);
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        imageView = findViewById(R.id.imageView);
-//        textView = findViewById(R.id.textView7);
-//
-//        imageView.animate().alpha(0f).setDuration(0);
-//        textView.animate().alpha(0f).setDuration(0);
-//
-//        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
-//            @Override
-//            public void onAnimationEnd(Animator animation) {
-//                textView.animate().alpha(1f).setDuration(800);
-//            }
-//        });
-
-
         intent = getIntent();
 
         Easy = findViewById(R.id.easy);
@@ -77,6 +88,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+            }
+        },3000);
+
 
     }
+
+
+//    new Handler().postDelayed(new Runnable() {
+//        @Override
+//        public void run() {
+//
+//
+//                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//
+//
+//        }
+//    },3000);
 }
